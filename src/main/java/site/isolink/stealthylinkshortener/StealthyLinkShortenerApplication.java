c112147efa7baded342e9a7fb374c755e8594a6e
Maria Malysheva <mn.malysheva@mail.ru>
2022-08-16 14:32:24 +0300

    Initialize Spring Boot project

---
+++ src/main/java/site/isolink/stealthylinkshortener/StealthyLinkShortenerApplication.java
@@ -0,0 +1,13 @@
+package site.isolink.stealthylinkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StealthyLinkShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StealthyLinkShortenerApplication.class, args);
+	}
+
+}
