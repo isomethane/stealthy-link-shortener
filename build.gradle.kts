8e32927bbb0ebaa0f61d638fca76d639a456695b
Maria Malysheva <mn.malysheva@mail.ru>
2022-08-16 16:35:49 +0300

    Switch to Kotlin DSL in build script

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.2"
+	id("io.spring.dependency-management") version "1.0.12.RELEASE"
+}
+
+group = "site.isolink"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
