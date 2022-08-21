f873ba4c2f18e035a0589c2714bc79b5795c9072
Maria Malysheva <mn.malysheva@mail.ru>
2022-08-21 20:45:27 +0300

    Remove hidden.properties property source

--- src/main/java/site/isolink/stealthylinkshortener/StealthyLinkShortenerApplication.java
+++ src/main/java/site/isolink/stealthylinkshortener/StealthyLinkShortenerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @PropertySource("classpath:public.properties")
-@PropertySource("classpath:hidden.properties")
 public class StealthyLinkShortenerApplication {
 	/**
 	 * Application main function.
