fbe513f952621ee1f4d02efe99957472937941a4
Maria Malysheva <mn.malysheva@mail.ru>
2022-12-02 03:24:23 +0400

    Redirect http requests to https

--- src/main/java/site/isolink/stealthylinkshortener/SecurityConfiguration.java
+++ src/main/java/site/isolink/stealthylinkshortener/SecurityConfiguration.java
@@ -1,7 +1,12 @@
 package site.isolink.stealthylinkshortener;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -45,5 +50,35 @@ public class SecurityConfiguration {
                 .httpBasic()
             .and()
             .build();
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer(
+        @Value("${server.http.port}") int httpPort,
+        @Value("${server.port}") int httpsPort
+    ) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector(httpPort, httpsPort));
+        return tomcat;
+    }
+
+    private Connector redirectConnector(int httpPort, int httpsPort) {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+
+        return connector;
     }
 }
